6f90aa31a2c6925c2cce3ba72a19ee5f9c114475
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-04-24 15:20:01 +0900

    商品購入機能：formオブジェクト構築

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,30 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+  end
+
+  validates :prefecture_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      building: building,
+      phone_number: phone_number,
+      addresses: addresses,
+      order_id: order.id
+    )
+  end
+end
