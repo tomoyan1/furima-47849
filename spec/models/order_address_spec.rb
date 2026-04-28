d12b6701b112a083289e92ecc94fcf0fd4e0ca29
tomoyan1 <133628017+tomoyan1@users.noreply.github.com>
2026-04-28 14:36:23 +0900

    商品購入機能（テストコード修正）

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
-      @order_address = FactoryBot.build(:order_address, user_id: 1, item_id: 1)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item, user_id: user.id)
+
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
     end
 
     context '内容が正しく入力されているとき' do
